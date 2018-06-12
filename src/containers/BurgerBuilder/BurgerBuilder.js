f85c096d8d058359f2d5916e48f3e7d67924a30d
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-12 11:58:12 -0700

    chore: add action creators and rearrange store folder structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axiosInstance from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions';
+import * as actions from '../../store/actions/actions';
 
 class BurgerBuilder extends Component {
     state = {
@@ -113,14 +113,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingName ) => dispatch({ 
-            type: actionTypes.ADD_INGREDIENT,
-            ingredientName: ingName 
-        }),
-        onIngredientRemoved: (ingName ) => dispatch({ 
-            type: actionTypes.REMOVE_INGREDIENT,
-            ingredientName: ingName 
-        }),
+        onIngredientAdded: (ingName ) => dispatch(actions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName ) => dispatch(actions.removeIngredient(ingName))
     }
 }
 
---
+++ src/store/actions/actBurgerBuilder.js
@@ -0,0 +1,16 @@
+
+import * as actionTypes from './actTypes';
+
+export const addIngredient = (name) => {
+    return {
+        type: actionTypes.ADD_INGREDIENT,
+        ingredientName: name
+    }
+}
+
+export const removeIngredient = (name) => {
+    return {
+        type: actionTypes.REMOVE_INGREDIENT,
+        ingredientName: name
+    }
+}
