0a18f2083a026c9c2b9c1fbe38991b60b8d6d41e
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-12 18:17:28 -0700

    ref: use helper functions to create a lean switch statement

--- src/store/reducers/redBurgerBuilder.js
+++ src/store/reducers/redBurgerBuilder.js
@@ -16,54 +16,60 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 };
 
+const addIngredient = (state, action) => {
+    //updated using a utitilty function
+    const updatedIngredient = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1 //ES6
+    }
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    }
+
+    return updateObject(state, updatedState);
+}
+
+const removeIngredient = (state, action) => {
+    return {
+        ...state, //copy layer 1 of object
+        ingredients: {
+            ...state.ingredients, //copy layer 2 of object
+            [action.ingredientName]: state.ingredients[action.ingredientName] - 1 //ES6
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+    };
+}
+
+const fetchIngredients = (state, action) => {
+    return {
+        ...state,
+        ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat,
+        },
+        totalPrice: 4,
+        error: false
+    };
+}
+
+const fetchIngredientsFailed = (state, action) => {
+    //updated using a utitilty function
+    return updateObject(state, {error: true});
+}
 
 const reducer = (state = initialState, action) => {
 
     switch(action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            
-            //updated using a utitilty function
-            const updatedIngredient = {
-                [action.ingredientName]: state.ingredients[action.ingredientName] + 1 //ES6
-            }
-            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-
-            return updateObject(state, updatedState);
-
-        case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state, //copy layer 1 of object
-                ingredients: {
-                    ...state.ingredients, //copy layer 2 of object
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1 //ES6
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-            };
-
-        case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: {
-                    salad: action.ingredients.salad,
-                    bacon: action.ingredients.bacon,
-                    cheese: action.ingredients.cheese,
-                    meat: action.ingredients.meat,
-                },
-                totalPrice: 4,
-                error: false
-            };
-
-        case actionTypes.FETCH_INGREDIENTS_FAILED:
-            //updated using a utitilty function
-            return updateObject(state, {error: true});
-
-        default: 
-            return state;
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);  
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);   
+        case actionTypes.SET_INGREDIENTS: return fetchIngredients(state, action);
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action);
+        default: return state;
     }
+    
 };
 
 export default reducer;
