ecad2ccfb926d2347119cf30443c49571f3d296a
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-14 13:59:55 -0700

    test: add redux reducers testing for redAuth

---
+++ src/store/reducers/redAuth.test.js
@@ -0,0 +1,35 @@
+
+import redAuth from './redAuth';
+import * as actionTypes from '../actions/actTypes';
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(redAuth(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+
+    it('should store the toke upon login', () => {
+        expect(redAuth({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'some-token',
+            userId: 'some-user-id'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    })
+});
